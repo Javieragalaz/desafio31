a7a33be5b22c116da346212baca1085fc2f6fb49
Javieragalaz <jgalazbecerra@gmail.com>
2023-08-06 09:38:28 -0400

    final

--- consultas.js
+++ consultas.js
@@ -1,15 +1,17 @@
 const { Pool } = require('pg') //CLASE POOL: multiconexiones y mejor rendimiento
 
 const pool = new Pool({
+//Credenciales psql
 host: 'localhost',
 user: 'javi',
 password: 'admin',
+
 database: 'likeme',
 port: 5432,
 allowExitOnIdle: true //cerrar la conexión luego de cada consulta
 })
 
-const agregarPost = async (titulo, url, descripcion) => {  
+const addPost = async (titulo, url, descripcion) => {  
     const consulta = "INSERT INTO posts VALUES (DEFAULT, $1, $2, $3)"
     const values = [titulo, url, descripcion]
     const result = await pool.query(consulta, values)
@@ -17,10 +19,10 @@ const agregarPost = async (titulo, url, descripcion) => {
     return result 
     }
     
-const obtenerPost = async () => {
+const getPost = async () => {
     const { rows } = await pool.query("SELECT * FROM posts")
     console.log(rows)
     return rows
 }
 
-module.exports = {agregarPost, obtenerPost};
+module.exports = {addPost, getPost};
--- server.js
+++ server.js
@@ -1,35 +1,37 @@
-const { agregarPost, obtenerPost } = require("./consultas");
+const { addPost, getPost } = require("./consultas");
 const express = require('express');
 const cors = require('cors');
 
-const app = express();
 
 var bodyParser = require('body-parser') //middleware
-
+const app = express();
 
 const PORT = 3000
 app.listen(PORT, () => {
-    console.log(`Servidor encendido en puerto: http://localhost:${PORT}`);
+    console.log(`Servidor encendido en puerto ${PORT}`);
 });
 
+//permite acceder a la información de un payload
 app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({
+  bodyParser.urlencoded({ // middleware para convertir datos a JSON
     extended: true,
   }),
 );
 
+//Intercambio de recursos de origen cruzado: Permite la comunicación de 2 aplicaciones de orígenes o dominios diferentes
 app.use(cors());
 
-
+//método para entregar información a partir de una consulta
 app.get("/posts", async (req, res) =>{
-    const posts = await obtenerPost()
+    const posts = await getPost()
     res.json(posts)
 });
 
+
 app.post("/posts", async (req, res) => {
-    const { titulo, url, descripcion } = req.body
+    const { titulo, url, descripcion } = req.body //almacenando en una constante el payload
     console.log(req.body)
-    await agregarPost(titulo, url, descripcion)
-    res.send("Post agregado con exito")
+    await addPost(titulo, url, descripcion) 
+    res.send("Post agregado con exito") 
 })
